e96d08f7303e9e2b3b9ae815dd9b3cb649aebe34
praveen7101994 <30315923+praveen7101994@users.noreply.github.com>
2019-02-18 21:58:29 +0530

    parsing request paths
    parsing request paths

--- index.js
+++ index.js
@@ -5,9 +5,21 @@
 
 // Dependencies
 var http = require('http');
+var url = require('url');
 // the server should respond to all request with a strin
 var server = http.createServer(function(req, res){
+    // Get the URL and parse it
+    var parsedUrl = url.parse(req.url,true);
+
+    // Get the path from url
+    var path = parsedUrl.pathname;
+    var trimmedPath = path.replace(/^\/+|\/+$/g,'');
+
+    // Send the response
+    
     res.end('Hello World\n');
+    // Log the request path
+    console.log('Request received on path: '+trimmedPath);
 });
 
 //
