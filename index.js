a420c9c9086cdda775e1c159d3a55d35acda8925
praveen7101994 <30315923+praveen7101994@users.noreply.github.com>
2019-02-18 21:34:07 +0530

    12. create and listen to the server on port 3000
    12. create and listen to the server on port 3000

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-console.log("hello haricomputerlines.com is here");
-console.log("first commit from github desktop");
+/*
+* primary file for the API
+*
+*/
+
+// Dependencies
+var http = require('http');
+// the server should respond to all request with a strin
+var server = http.createServer(function(req, res){
+    res.end('Hello World\n');
+});
+
+//
+server.listen(3000,function(){
+    console.log("The server is listening on port 3000 now");
+});
