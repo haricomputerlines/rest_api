091a73843c01345083a6514868d9b0a859f3396f
praveen7101994 <30315923+praveen7101994@users.noreply.github.com>
2019-02-18 22:07:42 +0530

    parsing http method
    parsing http method

--- index.js
+++ index.js
@@ -15,11 +15,15 @@ var server = http.createServer(function(req, res){
     var path = parsedUrl.pathname;
     var trimmedPath = path.replace(/^\/+|\/+$/g,'');
 
+    // Get the http Method
+    var method = req.method.toLowerCase();
+
     // Send the response
     
     res.end('Hello World\n');
+    
     // Log the request path
-    console.log('Request received on path: '+trimmedPath);
+    console.log('Request received on path: '+trimmedPath+ ' with method: ' + method);
 });
 
 //
